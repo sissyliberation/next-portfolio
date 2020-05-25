ca25217dc7202f0fc6f6cf6587ed18d1506b0cd0
nova skye <courtois1337@gmail.com>
2020-05-25 17:48:30 -0500

    style hero

--- components/hero/index.js
+++ components/hero/index.js
@@ -1,25 +1,31 @@
 import styles from './hero.module.scss'
 
-const hero = {
-  title: 'hi, im nova skye',
-  text: [
-    'my pronouns are <span classNmae="hero__accent">they</span> or <span className="hero__accent">she</span>.',
-    'i make things. i try not to make things worse.',
-  ],
-};
+
 
 console.log(styles);
 
 export default function Hero() {
+  const hero = {
+    title: 'im nova skye',
+    text: [
+      `my pronouns are <span class=${styles.pronouns}>they</span> or <span class=${styles.pronouns}>she</span>`,
+      'i make things',
+      'i try not to make things worse'
+    ],
+  };
+
   return (
     <section className={styles.hero}>
-      <div className="hero-container">
-        <h1 className="hero-title">{hero.title}</h1>
-        <div className="hero-text" dangerouslySetInnerHTML={{
+      <div className={styles.container}>
+        <h1 className={styles.title}>{hero.title}</h1>
+        <div className={`${styles.text} ${styles.text1}`} dangerouslySetInnerHTML={{
           __html: hero.text[0]
         }}></div>
-        <div className="hero-text" dangerouslySetInnerHTML={{
+        <div className={`${styles.text} ${styles.text2}`} dangerouslySetInnerHTML={{
           __html: hero.text[1]
+        }}></div>
+        <div className={`${styles.text} ${styles.text3}`} dangerouslySetInnerHTML={{
+          __html: hero.text[2]
         }}></div>
       </div>
     </section>
