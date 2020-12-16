8dfe087faa8782322781a801edee85f483adf863
nova <courtois1337@gmail.com>
2020-12-15 17:35:30 -0800

    update twitter

--- components/footer/index.js
+++ components/footer/index.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <a className={styles.link} href="https://github.com/novacourtois" target="_blank">github</a>
+        <a className={styles.link} href="https://github.com/anarchotechslut" target="_blank">github</a>
 
         <a className={styles.link} href="mailto:novaskye@protonmail.com">email</a>
       </div>
